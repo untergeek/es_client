e785d59d3d39104fc87859f29c2f5e7324a056c3
untergeek <aaron@mildensteins.com>
2025-09-19 16:00:45 -0600

    Release prep for v8.19.0

--- src/es_client/__init__.py
+++ src/es_client/__init__.py
@@ -19,7 +19,7 @@ Example Usage:
     # Outputs debug message at level 1, if logging is configured appropriately.
 
 Version:
-    8.18.2
+    8.19.0
 """
 
 from datetime import datetime
@@ -33,7 +33,7 @@ if now.year == FIRST_YEAR:
 else:
     COPYRIGHT_YEARS = f"2025-{now.year}"
 
-__version__ = "8.18.2"
+__version__ = "8.19.0"
 __author__ = "Aaron Mildenstein"
 __copyright__ = f"{COPYRIGHT_YEARS}, {__author__}"
 __license__ = "Apache 2.0"
