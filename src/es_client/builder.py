a8af523fa743c083ff282bd689aabbdf891028c6
untergeek <aaron@mildensteins.com>
2025-10-07 07:45:47 -0600

    Address out of order assignment in builder class

--- src/es_client/builder.py
+++ src/es_client/builder.py
@@ -156,11 +156,11 @@ class Builder:
         version_max: t.Tuple = VERSION_MAX,
     ):
         debug.lv2('Initializing Builder object...')
+        self.attributes = DotMap()
         self.config = DotMap()
         self.config.client = DotMap()
         self.config.client.hosts = []
         self.config.other_settings = DotMap()
-        self.attributes = DotMap()
         self._secrets = SecretStore()
         self.set_client_defaults()
         self.set_other_defaults()
